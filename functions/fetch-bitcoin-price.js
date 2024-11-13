899f3e9fb746f7de1fda4e36498a988c859c889e
Artem Klibaner <32778606+z97@users.noreply.github.com>
2024-11-13 12:13:52 +0200

    Update fetch-bitcoin-price.js

--- functions/fetch-bitcoin-price.js
+++ functions/fetch-bitcoin-price.js
@@ -1,51 +1,45 @@
 // functions/fetch-bitcoin-price.js
 
-let cachedPrice = null;
-let lastFetchTime = 0;
-const CACHE_DURATION = 10 * 60 * 1000; // 10 minutes
-
 exports.handler = async function(event, context) {
-  const currentTime = Date.now();
+  const apiKey = process.env.COINMARKETCAP_API_KEY;
+  const apiUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
+  const symbol = 'BTC';
 
-  if (cachedPrice && (currentTime - lastFetchTime) < CACHE_DURATION) {
-    // Serve from cache
+  try {
+    const response = await fetch(`${apiUrl}?symbol=${symbol}`, {
+      method: 'GET',
+      headers: {
+        'X-CMC_PRO_API_KEY': apiKey,
+        'Accept': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    // Access the price data
+    const price = data.data[symbol].quote.USD.price.toFixed(2);
+
+    // Return the price
     return {
       statusCode: 200,
-      body: JSON.stringify({ price: cachedPrice }),
+      body: JSON.stringify({ price }),
       headers: {
         'Content-Type': 'application/json',
       },
     };
-  } else {
-    // Fetch new data from CoinDesk API
-    try {
-      const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice/USD.json');
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      const price = data.bpi.USD.rate_float.toFixed(2);
-
-      // Update cache
-      cachedPrice = price;
-      lastFetchTime = currentTime;
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ price }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-    } catch (error) {
-      console.error('Error fetching Bitcoin price:', error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'Error fetching Bitcoin price' }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-    }
+  } catch (error) {
+    console.error('Error fetching Bitcoin price:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error fetching Bitcoin price' }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
   }
 };
+
